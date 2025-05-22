d38884cea3cc98a4d0f5ffa05de1a1d3f1edde61
Ilpo Juvander <ilpo@juvander.me>
2025-05-22 11:35:27 +0300

    fix

--- components/truma_inetbox/climate/TrumaRoomClimate.h
+++ components/truma_inetbox/climate/TrumaRoomClimate.h
@@ -9,30 +9,17 @@ namespace esphome {
   
   class TrumaRoomClimate : public Component, public climate::Climate, public Parented<TrumaiNetBoxApp> {
    public:
-    void setup() override {}
     void loop() override {}
     void dump_config() override;
     void control(const climate::ClimateCall &call) override;
   
-    climate::ClimateTraits traits() override {
-      auto traits = climate::ClimateTraits();
-      traits.set_supported_modes(this->supported_modes_);
-      traits.set_visual_min_temperature(this->visual_min_temperature_);
-      traits.set_visual_max_temperature(this->visual_max_temperature_);
-      traits.set_visual_temperature_step(this->visual_temperature_step_);
-      return traits;
-    }
-  
-    void set_supported_modes(std::vector<climate::ClimateMode> modes) {
-      this->supported_modes_ = std::move(modes);
-    }
   
     void set_visual_min_temperature(float value) { this->visual_min_temperature_ = value; }
     void set_visual_max_temperature(float value) { this->visual_max_temperature_ = value; }
     void set_visual_temperature_step(float value) { this->visual_temperature_step_ = value; }
   
    protected:
-    std::vector<climate::ClimateMode> supported_modes_;
+    std::set<ClimateMode> supported_modes_;
     float visual_min_temperature_{5.0};
     float visual_max_temperature_{30.0};
     float visual_temperature_step_{0.5};
