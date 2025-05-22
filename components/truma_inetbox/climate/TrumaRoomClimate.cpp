d13f25401643e8d7d09ead94549bf2ef4dee24a4
Ilpo Juvander <ilpo@juvander.me>
2025-05-22 11:52:45 +0300

    fix

--- components/truma_inetbox/climate/TrumaRoomClimate.cpp
+++ components/truma_inetbox/climate/TrumaRoomClimate.cpp
@@ -132,6 +132,7 @@ climate::ClimateTraits TrumaRoomClimate::traits() {
   auto traits = climate::ClimateTraits();
   traits.set_supports_current_temperature(true);
   traits.set_supported_modes({climate::ClimateMode::CLIMATE_MODE_OFF, climate::ClimateMode::CLIMATE_MODE_HEAT});
+  
   traits.set_supported_fan_modes({{
       climate::CLIMATE_FAN_OFF,
       climate::CLIMATE_FAN_LOW,
