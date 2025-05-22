55e063226675b5bc0e7106389eebcc65020157c6
Ilpo Juvander <ilpo@juvander.me>
2025-05-22 11:15:06 +0300

    fix

--- components/truma_inetbox/climate/__init__.py
+++ components/truma_inetbox/climate/__init__.py
@@ -1,3 +1,4 @@
+from esphome import automation
 from esphome.components import climate
 import esphome.config_validation as cv
 import esphome.codegen as cg
@@ -17,11 +18,9 @@ CLIMATE_MODES = {
     "AUTO": ClimateMode.CLIMATE_MODE_AUTO,
 }
 CLIMATE_VISUAL_SCHEMA = cv.Schema({
-    cv.Optional("target_temperature", default={}): cv.Schema({
         cv.Optional("min_temperature", default=5.0): cv.float_,
         cv.Optional("max_temperature", default=30.0): cv.float_,
         cv.Optional("temperature_step", default=0.5): cv.float_,
-    })
 })
 from .. import truma_inetbox_ns, CONF_TRUMA_INETBOX_ID, TrumaINetBoxApp
 
